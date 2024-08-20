d22c7e465675e6a05fbcb32f728e1f1a00f93d37
Dhiwin Samrich <109640171+dhiwinsamrich@users.noreply.github.com>
2024-08-20 21:17:26 +0530

    Add files via upload

---
+++ src/components/DoraAIATravelAgencyNamedZyraTr/DoraAIATravelAgencyNamedZyraTr.tsx
@@ -0,0 +1,283 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../_resets.module.css';
+import classes from './DoraAIATravelAgencyNamedZyraTr.module.css';
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 1:2 */
+export const DoraAIATravelAgencyNamedZyraTr: FC<Props> = memo(function DoraAIATravelAgencyNamedZyraTr(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.footer}>
+        <div className={classes.logoColumn}>
+          <div className={classes.logoFooter}></div>
+          <div className={classes.logoText}>
+            <div className={classes.textBlock}>© 2024 Zyra Travels, Inc.</div>
+            <div className={classes.textBlock2}>All rights reserved.</div>
+          </div>
+        </div>
+        <div className={classes.columnGroup}>
+          <div className={classes.linkColumn1}>
+            <div className={classes.linkColumnTitle1}>Company</div>
+            <div className={classes.linkColumnItem11}>About Us</div>
+            <div className={classes.linkColumnItem12}>Careers</div>
+            <div className={classes.linkColumnItem13}>Partners</div>
+            <div className={classes.linkColumnItem14}>FAQs</div>
+          </div>
+          <div className={classes.linkColumn2}>
+            <div className={classes.linkColumnTitle2}>Explore</div>
+            <div className={classes.linkColumnItem21}>Destinations</div>
+            <div className={classes.linkColumnItem22}>Packages</div>
+            <div className={classes.linkColumnItem23}>Experiences</div>
+            <div className={classes.linkColumnItem24}>Reviews</div>
+          </div>
+          <div className={classes.linkColumn3}>
+            <div className={classes.linkColumnTitle3}>Connect</div>
+            <div className={classes.linkColumnItem31}>Facebook</div>
+            <div className={classes.linkColumnItem32}>Instagram</div>
+            <div className={classes.linkColumnItem33}>Twitter</div>
+            <div className={classes.linkColumnItem34}>LinkedIn</div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.section9}>
+        <div className={classes.container45}>
+          <div className={classes.text}>
+            <p className={classes.labelWrapper}>Elevate Your Travels with Zyra</p>
+          </div>
+          <div className={classes.heading7}>
+            <p className={classes.labelWrapper2}>Connecting You to Exceptional Experiences</p>
+          </div>
+        </div>
+        <div className={classes.container41}>
+          <div className={classes.container42}>
+            <div className={classes.heading6}>
+              <p className={classes.labelWrapper3}>Explore Our Services</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.section8}>
+        <div className={classes.container39}>
+          <div className={classes.text2}>
+            <p className={classes.labelWrapper4}>Unlock the Potential of Crypttres</p>
+          </div>
+          <div className={classes.text3}>
+            <p className={classes.labelWrapper5}>
+              Immerse yourself in the captivating world of Crypttres, where the boundaries between technology and wonder
+              blur. Zyra Travels invites you to embark on a journey that will redefine your understanding of the
+              extraordinary
+            </p>
+          </div>
+        </div>
+        <div className={classes.container36}>
+          <div className={classes.container37}>
+            <button className={classes.button7}>
+              <div className={classes.text4}>
+                <p className={classes.labelWrapper6}>Join the Adventure</p>
+              </div>
+            </button>
+            <div className={classes.container38}>
+              <div className={classes.text5}>
+                <p className={classes.labelWrapper7}>Unravel the Mysteries</p>
+              </div>
+              <div className={classes.text6}>
+                <p className={classes.labelWrapper8}>Discover the Future</p>
+              </div>
+            </div>
+          </div>
+          <button className={classes.button6}>
+            <div className={classes.text7}>
+              <p className={classes.labelWrapper9}>Explore Now</p>
+            </div>
+          </button>
+          <div className={classes.heading5}>
+            <p className={classes.labelWrapper10}>Unlock the Secrets of Crypttres with Zyra Travels</p>
+          </div>
+        </div>
+      </div>
+      <div className={classes.section7}>
+        <div className={classes.container24}>
+          <div className={classes.container34}>
+            <div className={classes.image7}></div>
+            <div className={classes.image6}></div>
+          </div>
+          <div className={classes.container25}>
+            <div className={classes.container29}>
+              <div className={classes.container31}>
+                <div className={classes.container33}>
+                  <div className={classes.text8}>
+                    <p className={classes.labelWrapper11}>Elevate Your Travels</p>
+                  </div>
+                  <div className={classes.text9}>
+                    <p className={classes.labelWrapper12}>Plan Your Getaway</p>
+                  </div>
+                </div>
+                <div className={classes.container32}>
+                  <div className={classes.text10}>
+                    <p className={classes.labelWrapper13}>Experience the Extraordinary with Zyra Travels</p>
+                  </div>
+                  <div className={classes.text11}>
+                    <p className={classes.labelWrapper14}>Discover the World</p>
+                  </div>
+                </div>
+              </div>
+              <div className={classes.container30}>
+                <button className={classes.button5}>
+                  <div className={classes.text12}>
+                    <p className={classes.labelWrapper15}>Explore Destinations</p>
+                  </div>
+                </button>
+              </div>
+            </div>
+            <div className={classes.container26}>
+              <div className={classes.container28}>
+                <button className={classes.button4}>
+                  <div className={classes.text13}>
+                    <p className={classes.labelWrapper16}>Book Your Adventure</p>
+                  </div>
+                </button>
+              </div>
+              <div className={classes.container27}>
+                <div className={classes.text14}>
+                  <p className={classes.labelWrapper17}>
+                    Indulge in the ultimate travel luxury with our premium packages. Tailor-made for the discerning
+                    traveler, our offerings seamlessly blend comfort, adventure, and unforgettable moments that will
+                    leave you awestruck
+                  </p>
+                </div>
+                <div className={classes.text15}>
+                  <p className={classes.labelWrapper18}>
+                    From serene beaches to towering mountains, our curated travel experiences unlock the full potential
+                    of your wanderlust
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.container23}>
+          <div className={classes.heading4}>
+            <p className={classes.labelWrapper19}>Unleash the Possibilities with Zyra Travels</p>
+          </div>
+        </div>
+      </div>
+      <div className={classes.container4}>
+        <div className={classes.section6}>
+          <div className={classes.container18}>
+            <div className={classes.imageItem}>
+              <div className={classes.image5}></div>
+              <div className={classes.container21}>
+                <div className={classes.text16}>Embrace the Serenity of Waterways</div>
+              </div>
+            </div>
+            <div className={classes.imageItem2}>
+              <div className={classes.image4}></div>
+              <div className={classes.container20}>
+                <div className={classes.text17}>Immerse in Natural Splendor</div>
+              </div>
+            </div>
+            <div className={classes.imageItem3}>
+              <div className={classes.image3}></div>
+              <div className={classes.container19}>
+                <div className={classes.text18}>Explore Stunning Landscapes</div>
+              </div>
+            </div>
+          </div>
+          <div className={classes.container17}>
+            <div className={classes.text19}>
+              <p className={classes.labelWrapper20}>Embark on Unforgettable Journeys</p>
+            </div>
+            <div className={classes.heading3}>
+              <p className={classes.labelWrapper21}>Discover Your Next Adventure</p>
+            </div>
+          </div>
+        </div>
+        <div className={classes.section5}></div>
+        <div className={classes.section4}>
+          <div className={classes.container15}>
+            <div className={classes.heading2}>
+              <p className={classes.labelWrapper22}>Unlock the World&#39;s Wonders</p>
+            </div>
+          </div>
+          <div className={classes.container14}>
+            <div className={classes.text20}>
+              <p className={classes.labelWrapper23}>Elevate Your Travel Experience</p>
+            </div>
+          </div>
+        </div>
+        <div className={classes.section3}>
+          <div className={classes.container10}>
+            <div className={classes.container12}>
+              <div className={classes.text21}>
+                <p className={classes.labelWrapper24}>Plan Your Trip</p>
+              </div>
+            </div>
+            <div className={classes.container11}>
+              <div className={classes.text22}>
+                <p className={classes.labelWrapper25}>Discover our Destinations</p>
+              </div>
+            </div>
+          </div>
+          <div className={classes.container9}>
+            <button className={classes.button3}>
+              <div className={classes.text23}>
+                <p className={classes.labelWrapper26}>Learn More</p>
+              </div>
+            </button>
+          </div>
+        </div>
+        <div className={classes.section2}>
+          <div className={classes.container6}>
+            <div className={classes.image1}></div>
+            <div className={classes.container7}>
+              <button className={classes.button2}>
+                <div className={classes.text24}>
+                  <p className={classes.labelWrapper27}>Get Started</p>
+                </div>
+              </button>
+            </div>
+          </div>
+          <div className={classes.imageItem4}></div>
+          <div className={classes.imageItem5}></div>
+        </div>
+      </div>
+      <div className={classes.section1}>
+        <div className={classes.container2}>
+          <div className={classes.text25}>
+            <p className={classes.labelWrapper28}>
+              Discover the beauty of the world with our top-notch travel packages. Explore destinations, book flights,
+              and experience the adventure of a lifetime
+            </p>
+          </div>
+          <div className={classes.heading1}>
+            <p className={classes.labelWrapper29}>Explore the World with Zyra Travels</p>
+          </div>
+        </div>
+        <button className={classes.button1}>
+          <div className={classes.text26}>
+            <p className={classes.labelWrapper30}>Book Now</p>
+          </div>
+        </button>
+      </div>
+      <div className={classes.nav}>
+        <div className={classes.logoItem}>
+          <div className={classes.logoNav}></div>
+          <div className={classes.logoText2}>Zyra Travels</div>
+        </div>
+        <div className={classes.itemGroup}>
+          <div className={classes.itemContent}>Home</div>
+          <div className={classes.itemContent2}>About</div>
+          <div className={classes.itemContent3}>Services</div>
+          <div className={classes.itemContent4}>Contact</div>
+          <button className={classes.navButton}>
+            <div className={classes.navButtonContent}>Chat with Us</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+});
